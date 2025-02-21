166620c477817bf6b2fb9a52b5461c22d8bc891e
Juan Pablo Garcia Ceballos <1104806764@u.icesi.edu.co>
2025-02-21 13:02:18 -0500

    entrega

--- src/ui/Atlas.java
+++ src/ui/Atlas.java
@@ -1,8 +1,10 @@
 /*
 * Analisis
-* Descripcion del programa:
-* Entradas:
-* Salidas: 
+* Descripcion del programa: calcular el costo y elaborar las facturas de cada paquete de viaje vendido.
+
+
+* Entradas: nombre, edad, documento de identidad, ciudad de la que esta cotizando el viaje
+* Salidas: total de tiquetes, nombre,edad, identficacion
 * Ejemplo:
 */
 
@@ -16,6 +18,7 @@ public class Atlas {
 
     // Declaracion de constantes (completar)
     final double COSTO_TRAYECTO_AVION = 250000;
+    final double COSTO_NOCHE = 150000;
 
 	private Atlas() {
 		escaner = new Scanner(System.in);
@@ -25,21 +28,52 @@ public class Atlas {
 	public void run()
 	{   
         // Declaracion de info a pedir del usuario (completar)
-        String nombre;
+        String nombre,ciudad;
+        int edad, id,noches;
+        double totalest;
         // Saludo
         System.out.println("Bienvenido al programa de viajes EVENTOS ATLAS realizado por los estudiantes de Algoritmos y programacion I de la Universidad ICESI.");
         // Pedir info al usuario (completar)
         System.out.println("Cual es tu nombre?");
         nombre = escaner.nextLine();
+        System.out.println("Que edad tienes? ");
+        edad= escaner.nextInt();
+        System.out.println("Por favor escribe tu numero de identificacion ");
+        id=  escaner.nextInt();
+         System.out.println("Desde que ciudad se comunica con nosotros? ");
+       ciudad = escaner.nextLine();
+       escaner.nextLine();
+         System.out.println("Bienvenido a Atlas, cuantas noches deseas hospedarte? ");
+        noches= escaner.nextInt();
+System.out.println("Super! La vas a pasar genial. Te quedaras 2 noches en Bogota y este es el resumen de tu compra: ");
+
+
+
+        
 
         // ...
         System.out.println("Bienvenido " + nombre + "!");
+                
+          System.out.println("A nombre de: " + nombre + "!");
+          System.out.println("edad: " + edad + "!");
+          System.out.println("Documento: " + id + "!");
+          
+      
+
         // ...
 
         // Declaracion de salidas calculadas (completar)
-        double totalTransporte;
+        double totalTransporte, totest, totnoche, totestadia,costoser;
         // Calculo de salidas mediante metodos (completar)
         totalTransporte = calcularTotalTransporte(); // aqui podrian faltar parametros
+        totnoche= noches * COSTO_NOCHE;
+        
+        costoser= totnoche * 0.10;
+        totestadia= totnoche + costoser + COSTO_TRAYECTO_AVION;
+
+       
+        System.out.println(" Tu costo total es de" +  totestadia + "!");
+
 	}
 
 	public static void main(String[] args) {
@@ -55,7 +89,10 @@ public class Atlas {
 	 * @return 
 	 */
     public double calcularTotalTransporte() {
-        return 0; // Completar operacion
+       
+        return COSTO_TRAYECTO_AVION * 2;
+        
+         // Completar operacion
     }
 
 	
